f3233906b54b0e474f067df6bda9523002912207
dltorrise <dltorrise@gmail.com>
2023-02-27 15:05:01 -0600

    schema

--- schema.sql
+++ schema.sql
@@ -6,33 +6,34 @@ USE haroldsclownfactory_db;
 -- reordered tables to fix error happening with "chicken and egg problem" --
 -- can't create one table if part of it depends on creating another table --
 
-CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY, -- i'm not sure if I need to make this into a primary key
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT NOT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES roles(id)
-  ON DELETE SET NULL
+CREATE TABLE department (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
+  name VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE roles ( -- for some reason will not allow me to write 'role'
+CREATE TABLE role ( -- for some reason will not allow me to write 'role'
  -- in auto increment must set it as primary key on same line -- 
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(6, 2) NOT NULL,
   department_id INT,
---   PRIMARY KEY (id)
   FOREIGN KEY (department_id)
   REFERENCES deparment(id)
-  ON DELETE SET NULL
 );
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY, 
-  name VARCHAR(30) NOT NULL,
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- i'm not sure if I need to make this into a primary key
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
 );
+
+
+
+
 
 
 
